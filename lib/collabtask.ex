2cae9b56bcb393b8d1d041acecc1bbb40d33937d
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2025-07-10 00:51:46 +0100

    initial commit

---
+++ lib/collabtask.ex
@@ -0,0 +1,18 @@
+defmodule Collabtask do
+  @moduledoc """
+  Documentation for `Collabtask`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Collabtask.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/collabtask_test.exs
@@ -0,0 +1,8 @@
+defmodule CollabtaskTest do
+  use ExUnit.Case
+  doctest Collabtask
+
+  test "greets the world" do
+    assert Collabtask.hello() == :world
+  end
+end
